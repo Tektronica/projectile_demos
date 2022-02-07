3fa012d9638cd0a5d142ee223774d86e67592365
Tektronica <ryalho@gmail.com>
2022-02-07 15:55:58 -0800

    Updated demo_04 modelling the exit velocity for an air cannon. Currently, the model is over-simplified by not accounting for the flow rate of air through the valve connecting the reservoir and barrel. While the gas from a pressurized reservoir expands isothermally, adiabatic model produces a similar result since the temperature drop associated with the adiabatic expansion is so small.

--- demo_04_projectile_exit_velocity.py
+++ demo_04_projectile_exit_velocity.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# http://persweb.wabash.edu/facstaff/madsenm/publications/AJP_80_24_rohrbach_air_cannon.pdf
+# https://www.am1.us/wp-content/uploads/Documents/U24146_Potato_Cannon_and_Chronograph_Part_I.pdf
 # https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjT87rUten1AhU3IjQIHRM0BlMQFnoECAcQAQ&url=http%3A%2F%2Fpersweb.wabash.edu%2Ffacstaff%2Fmadsenm%2Fpublications%2FAJP_80_24_rohrbach_air_cannon.pdf&usg=AOvVaw3cb64OCiWA0-ciEPyqPfQR
 # http://persweb.wabash.edu/facstaff/madsenm/publications/AJP_80_24_rohrbach_air_cannon.pdf
 
@@ -17,41 +19,93 @@ rho_hydrogen = 0.0899  # Air density (kg.m-3) @ STP
 
 
 def run_demo():
-    # Initial speed and launch angle (from the horizontal).
-    launch_angle = 65  # deg
-    launch_velocity = 50  # m/s
-    launch_height = 1  # m
+    # Initial Conditions -----------------------------------------------------------------------------------------------
+    m = 19.40 * 1e-3  # mass [kg]
+    h = 4.8019 * 1e-2  # length of projectile [m]
 
-    run_projectile_exit_velocity(launch_angle, launch_velocity, launch_height)
+    x0 = 3.35 * 1e-2  # initial projectile position before launch [m]
+    xbarrel = 91.6 * 1e-2  # length of barrel [m]
+    L = xbarrel - x0  # travel distance [m]
+
+    D = 1.913 * 1e-2  # diameter of barrel [m]
+    A = np.pi * (D / 2) ** 2  # cross-sectional area of the barrel [m^2]
+
+    # cannon reservoir volume increases as the projectile moves down the barrel: V0 = A * x0
+    V0 = 4.196 * 1e-3  # initial air reservoir volume [m^3]
+    P0 = 200  # initial pressure of air reservoir [kPa]
+
+    gamma = 7 / 5  # diatomic ideal gasses
+    Patm = 101.325  # pressure at atmosphere [kPa]
+    Ff = 0  # force due to friction [N] or [kg·m·s-2] (acetal copolymer)
+
+    # VELOCITY AS A FUNCTION OF BARREL LENGTH --------------------------------------------------------------------------
+    x = np.linspace(0, L, 1000)  # [m]
+    yad_x = exit_velocity_adiabatic(x, P0, V0, A, m, gamma, Patm, Ff)  # [m/s]
+    yiso_x = exit_velocity_isothermal(x, P0, V0, A, m, Patm, Ff)  # [m/s]
+
+    # VELOCITY AS A FUNCTION OF RESERVOIR PRESSURE ---------------------------------------------------------------------
+    Pmin_ad = get_root_adiabatic(L, V0, A, gamma, Patm, Ff)  # minimum pressure in adiabatic expansion
+    Pmin_iso = get_root_isothermal(L, V0, A, Patm, Ff)  # minimum pressure in isothermal expansion
+
+    xfixed = L - x0
+    P0 = np.linspace(max(Pmin_ad, Pmin_iso), 800, 1000)  # x grid of pressures
+
+    yad_p = exit_velocity_adiabatic(xfixed, P0, V0, A, m, gamma, Patm, Ff)  # [m/s]
+    yiso_p = exit_velocity_isothermal(xfixed, P0, V0, A, m, Patm, Ff)  # [m/s]
+
+    # PLOT RESULTS -----------------------------------------------------------------------------------------------------
+    plot(x, [yad_x, yiso_x], P0, [yad_p, yiso_p])
 
 
-def run_projectile_exit_velocity(launch_angle, v0, h):
-    phi0 = np.radians(launch_angle)
-
-    # Initial conditions
-    x0 = 0
-    y0 = h
-    Vx0 = v0 * np.cos(phi0)
-    Vy0 = v0 * np.sin(phi0)
-
-    x, y = [1, 2], np.array([[1, 2], [2, 3]])
-    plot(x, y.T)
+def exit_velocity_adiabatic(x, P0, V0, A, m, g, Patm, Ff):
+    # for adiabatic expansion, has no heat transfer and temperature is reduced
+    P0, Patm = (P0 * 1e3), (Patm * 1e3)  # convert to base pascal units
+    return np.sqrt((2 / m) * (((P0 * V0) / (g - 1)) * (1 - (V0 / (V0 + A * x)) ** (g - 1)) - (Patm * A * x) - Ff * x))
 
 
-def plot(x, y):
-    fig, ax1 = plt.subplots()
-    if isinstance(x[0], list):
-        for i in range(len(x)):
-            plt.plot(x[i], y[i], label='id %s' % i)
-    else:
-        plt.plot(x, y)
+def exit_velocity_isothermal(x, P0, V0, A, m, Patm, Ff):
+    # for isothermal expansion, temperature remains constant and pressure is reduced
+    # The gas from a pressurized reservoir expands isothermally
+    P0, Patm = (P0 * 1e3), (Patm * 1e3)  # convert to base pascal units
+    return np.sqrt((2 / m) * (P0 * V0 * np.log(1 + ((A * x) / V0)) - (Patm * A * x) - Ff * x))
 
-    ax1.set_title('Projectile Demo with air resistance')
-    ax1.set_xlabel('distance (m)')
-    ax1.set_ylabel('height (m)')
-    ax1.legend(['$\\rho=0$', f'$\\rho={rho_air}$ (air)', f'$\\rho={rho_hydrogen}$ (hydrogen)'])
 
-    plt.grid()
+def exit_velocity_corrected(x, P0, V0, A, m, Patm, Ff):
+    # the corrected model takes into account the flow rate of air through the valve.
+    P0, Patm = (P0 * 1e3), (Patm * 1e3)  # convert to base pascal units
+    return np.sqrt((2 / m) * (P0 * V0 * np.log(1 + ((A * x) / V0)) - (Patm * A * x) - Ff * x))
+
+
+def get_root_adiabatic(x, V0, A, g, Patm, Ff):
+    # minimum pressure in adiabatic expansion
+    return ((1-g) * x * (-Patm * A - Ff)) / (V0 - (((V0 / (V0 + A * x)) ** g) * (A * x + V0)))
+
+
+def get_root_isothermal(x, V0, A, Patm, Ff):
+    # minimum pressure in isothermal expansion
+    return (x * (A * Patm + Ff)) / (V0 * np.log((A * x) / V0 + 1))
+
+
+def plot(x, y, x2, y2):
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
+    fig.suptitle('Compressed Air Cannon', fontsize=18)
+
+    ax1.plot(x * 1e2, np.asarray(y).T)
+
+    ax1.grid()
+    ax1.set_title('Exit Velocity as a function of barrel travel length')
+    ax1.set_xlabel('barrel length (travelled) (cm)')
+    ax1.set_ylabel('v (m/s)')
+    ax1.legend(['adiabatic', 'isothermal'])
+
+    ax2.plot(x2, np.asarray(y2).T)
+
+    ax2.grid()
+    ax2.set_title('Exit Velocity as a function of the initial pressure')
+    ax2.set_xlabel('initial pressure of air reservoir $P_0 (m)$')
+    ax2.set_ylabel('v (m/s)')
+
+    plt.tight_layout()
     plt.show()
 
 
